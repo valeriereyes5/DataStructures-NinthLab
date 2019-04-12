5379d5af75a9f2091486030ef8772de7ab3736df
Valerie Reyes Ortiz <valerie.reyes5@upr.edu>
2019-04-12 00:16:14 -0400

    Final

---
+++ src/testerClasses/IntegerComparator2.java
@@ -0,0 +1,13 @@
+package testerClasses;
+import java.util.Comparator;
+
+
+
+public class IntegerComparator2 implements Comparator<Integer>{
+
+	public int compare(Integer o2, Integer o1) {
+		return o1.compareTo(o2);
+	}
+
+
+}
